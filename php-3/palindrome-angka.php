d5cc7867c82e0a038beeaffb1ea2f1a71fb22986
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-29 20:47:19 +0800

    Update palindrome-angka.php

--- php-3/palindrome-angka.php
+++ php-3/palindrome-angka.php
@@ -2,10 +2,36 @@
 
 function palindrome_angka($angka) {
   // tulis kode di sini
-}
+	$result = "";
+	$strAngka = strval($angka);
+	$reversedString = "";
+	for ($i = strlen($strAngka) - 1; $i >= 0; $i--){
+		$reversedString .= $strAngka[$i];
+	}
+
+	if ($reversedString == $strAngka){
+		$angka += 1;
+	}
+
+	while (true) {
+		$strAngka = strval($angka);
+		$reversedString = "";
+		for ($i = strlen($strAngka) - 1; $i >= 0; $i--){
+			$reversedString.= $strAngka[$i];
+		}
+		if($reverseString == $strAngka){
+			global $result;
+			$result = $strAngka . '<br>';
+			return $result;
+		}
+		else{
+			$angka +=1;
+		};
+	};
+};
 
 // TEST CASES
-echo palindrome_angka(8) // 9
+echo palindrome_angka(8); // 9
 echo palindrome_angka(10); // 11
 echo palindrome_angka(117); // 121
 echo palindrome_angka(175); // 181
