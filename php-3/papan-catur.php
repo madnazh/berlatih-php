1c606d07434d75b8d6ac0eef6ee68132ee4893b1
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-29 20:50:38 +0800

    Update papan-catur.php

--- php-3/papan-catur.php
+++ php-3/papan-catur.php
@@ -1,7 +1,28 @@
 <?php
 
 function papan_catur($angka) {
-// tulis kode di sini
+// tulis kode di sini 
+ $text = '';
+ // Jumlah Kotak
+ $jumlah = "";
+ // Membuat Tag Table
+ $text .= '<table border=1>';
+ // Perulangan pembuatan kotak catur
+ for($n = 0; $n < $angka; $n++) {
+  $text .= '<tr>';
+  for($m = 0; $m < ($angka*2)-1; $m++) {
+   // Pemberian Warna
+   $warna = '';
+      if(($n % 2 == 0 && $m % 2 == 0) || ($n % 2 == 1 && $m % 2 == 1)) {
+          $warna = '#000';
+      }
+   else $warna = '#fff';
+      $text .= '<td width=30 height=30 bgcolor='.$warna.'></td>';
+  }
+  $text .= '</tr>';
+ }
+ // Tag Penutup Table
+ $text .= '</table>';
 }
 
 // TEST CASES
@@ -22,7 +43,8 @@ echo papan_catur(8)
 # # # # # # # #
  # # # # # # #
 # # # # # # # #
- # # # # # # #
+ # # # # # # # */
+
 
 echo papan_catur(5) 
 /*
@@ -32,3 +54,5 @@ echo papan_catur(5)
  # # # # 
 # # # # #
 */
+
+?>
