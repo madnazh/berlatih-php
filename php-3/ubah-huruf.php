20d8bb0bb806b1ba59496c21077f8f87873dd1b7
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-30 14:13:21 +0800

    Update ubah-huruf.php

--- php-3/ubah-huruf.php
+++ php-3/ubah-huruf.php
@@ -1,13 +1,20 @@
 <?php
 function ubah_huruf($string){
 //kode di sini
+	$huruf = "abcdefghijklmnopqrstuvwxyz";
+	$hasil = "";
+	for($i = 0; $i < strlen($string); $i++){
+		$position = strrpos($huruf, $string[$i]);
+		$hasil .= substr($huruf, $position + 1, 1);
+	}
+	return $hasil;
 }
 
 // TEST CASES
-echo ubah_huruf('wow'); // xpx
-echo ubah_huruf('developer'); // efwfmpqfs
-echo ubah_huruf('laravel'); // mbsbwfm
-echo ubah_huruf('keren'); // lfsfo
-echo ubah_huruf('semangat'); // tfnbohbu
+echo "<br>". ubah_huruf('wow'); // xpx
+echo "<br>". ubah_huruf('developer'); // efwfmpqfs
+echo "<br>". ubah_huruf('laravel'); // mbsbwfm
+echo "<br>". ubah_huruf('keren'); // lfsfo
+echo "<br>". ubah_huruf('semangat'); // tfnbohbu
 
 ?>
