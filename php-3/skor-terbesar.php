7273d00141a3a47991a4e32889a0cfb501569211
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-30 14:24:16 +0800

    Update skor-terbesar.php

--- php-3/skor-terbesar.php
+++ php-3/skor-terbesar.php
@@ -1,7 +1,37 @@
 <?php
 function skor_terbesar($arr){
 //kode di sini
-}
+  $array = [];
+  for ($i = 0; $i < count($arr); $i++) { 
+    array_push($keys, $arr[$i]['kelas']);
+    if($arr[$i]['kelas'] == array_unique($keys)[0]){
+      array_push($Laravel,$arr[$i]);
+    } else if($arr[$i]['kelas'] == array_unique($keys)[1]){
+      array_push($reactNative, $arr[$i]);
+    } else{
+      array_push($reactJs, $arr[$i]);
+    }
+  
+  }
+
+    $nilaiLaravel = array_column($Laravel, 'nilai');
+    rsort($nilaiLaravel);
+    $nilaiReactJs = array_column($Laravel, 'nilai');
+    rsort($nilaiReactJs);
+    $nilaiReactNative = array_column($Laravel, 'nilai');
+    rsort($nilaiReactNative);
+    $result = [];
+    $kelas = ['Laravel','React Js','React Native'];
+
+  for($j = 0; $j < count($arr); $j++){
+    if($arr[$j]['nilai'] == $nilaiLaravel[0] || $arr[$j]['nilai'] == $nilaiReactJs[0] || $arr[$j]['nilai'] == $nilaiReactNative[0]){
+      array_push($result, $arr[$j]);
+      }
+    }
+
+    $fix = array_combine($kelas, $result);
+    return $fix;
+  }
 
 // TEST CASES
 $skor = [
