95abe87f7668e3cde993322a90a37ff9fe0f479d
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-30 14:12:46 +0800

    Update tentukan-deret-geometri.php

--- php-3/tentukan-deret-geometri.php
+++ php-3/tentukan-deret-geometri.php
@@ -1,8 +1,22 @@
 <?php
 function tentukan_deret_geometri($arr) {
 // kode di sini
+	$pola = $arr[1] / $arr[0];
+	$banding = [];
+	$banding[0] = $arr[0];
+	for ($i = 1; $i <= sizeof($arr)- 1; $i++){
+		$banding[] = $arr[$i - 1] * $pola;
+	}
+
+	if (array_diff($banding, $arr)){
+		print_r($arr); echo "<b>false</b><br>";
+
+	} else {
+		print_r($arr); echo "<b>true</b><br>";
+	}
 }
 //TEST CASES
+echo "DERET GEOMETRI <br><br>";
 echo tentukan_deret_geometri([1, 3, 9, 27, 81]); // true
 echo tentukan_deret_geometri([2, 4, 8, 16, 32]); // true
 echo tentukan_deret_geometri([2, 4, 6, 8]); // false
